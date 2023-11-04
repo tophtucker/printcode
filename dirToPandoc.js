1e65b1f08c69d41c96410539f5081b8eb330b997
Toph Tucker <tophtucker@gmail.com>
2023-11-04 17:37:02 -0400

    esm

--- dirToPandoc.js
+++ dirToPandoc.js
@@ -1,11 +1,11 @@
 // https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
-const { resolve } = require("path");
-const { readdir } = require("fs").promises;
+import path from "path";
+import {readdir} from "fs/promises";
 
 async function* getFiles(dir) {
-  const dirents = await readdir(dir, { withFileTypes: true });
+  const dirents = await readdir(dir, {withFileTypes: true});
   for (const dirent of dirents) {
-    const res = resolve(dir, dirent.name);
+    const res = path.resolve(dir, dirent.name);
     if (dirent.isDirectory()) {
       yield* getFiles(res);
     } else {
