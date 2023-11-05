d9f2a318096f466af6ec3f961dabb393f9f14b4c
Toph Tucker <tophtucker@gmail.com>
2023-11-04 23:22:18 -0400

    clean up implementation

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-// https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
 import path from "path";
 import {readFile} from "fs/promises";
 import {glob} from "glob";
-
-// const allowedExtensions = ["js", "ts", "mjs", "json", "txt", "md"];
 
 const root = process.argv[2] || ".";
 const absoluteRoot = path.resolve(root);
 const title = absoluteRoot.split("/").filter(d => d.length).at(-1);
 
-let doc = `<html>
+// arbitrary initial ignore stuff, works for me, idk, i should let ppl pass this in
+let ignore = ["*.pdf", "package-lock.json", "yarn.lock", "*.png", "*.svg", "*.jpg"];
+try {
+  // respect .gitignore
+  ignore.push(...(await readFile(path.resolve(root, ".gitignore"), "utf8")).split("\n").filter(d => d));
+} catch (e) {
+  // there's no .gitignore, that's ok
+}
+
+// https://stackoverflow.com/a/30970751/120290
+function escape(s) {
+  let lookup = {'&': "&amp;", '"': "&quot;", '\'': "&apos;", '<': "&lt;", '>': "&gt;"};
+  return s.replace( /[&"'<>]/g, c => lookup[c] );
+}
+
+async function getFiles(dir, level = 2) {
+  const dirents = (await glob(dir + "/*", {cwd: absoluteRoot, ignore, withFileTypes: true}))
+    .sort((a, b) => a.name.localeCompare(b.name));
+  return `${await Promise.all(dirents.map(async dirent => {
+    const res = path.resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      return `<h${level}>${dirent.name}</h${level}>
+        ${await getFiles(res, level + 1)}`;
+    } else {
+      return `<h${level + 1}>${dirent.name}</h${level + 1}>
+        <pre>${escape(await readFile(res, "utf8"))}</pre>`;
+    }
+  }))}`
+}
+
+process.stdout.write(`<html>
 <title>${title}</title>
 <style>
 h1, h2, h3, h4, h5, h6 { margin-bottom: 1rem; }
@@ -22,44 +49,9 @@ h1 { font-size: 32rem; }
 </style>
 <body>
 
-<h1>${title}</h1>`;
+<h1>${title}</h1>
 
-// arbitrary initial ignore stuff, works for me, idk, i should let ppl pass this in
-let ignore = ["*.pdf", "package-lock.json", "yarn.lock", "*.png", "*.svg", "*.jpg"];
-try {
-  ignore.push(...(await readFile(path.resolve(root, ".gitignore"), "utf8")).split("\n").filter(d => d));
-} catch (e) {
-  // ignore
-}
+${await getFiles(root)}
 
-// https://stackoverflow.com/a/30970751/120290
-function escape(s) {
-  let lookup = {
-      '&': "&amp;",
-      '"': "&quot;",
-      '\'': "&apos;",
-      '<': "&lt;",
-      '>': "&gt;"
-  };
-  return s.replace( /[&"'<>]/g, c => lookup[c] );
-}
-
-async function getFiles(dir, level = 2) {
-  const dirents = (await glob(dir + "/*", {cwd: absoluteRoot, ignore, withFileTypes: true}))
-    .sort((a, b) => a.name.localeCompare(b.name));
-  for (const dirent of dirents) {
-    const res = path.resolve(dir, dirent.name);
-    if (dirent.isDirectory()) {
-      doc += `<h${level}>${dirent.name}</h${level}>`;
-      await getFiles(res, level + 1);
-    } else {
-      doc += `<h${level + 1}>${dirent.name}</h${level + 1}>
-        <pre>${escape(await readFile(res, "utf8"))}</pre>`;
-    }
-  }
-}
-
-await getFiles(root);
-
-doc += "</body></html>";
-process.stdout.write(doc);
+</body>
+</html>`);
