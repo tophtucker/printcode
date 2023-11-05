7da93f7e4d419a5620d3860480d3e183e8d4a04b
Toph Tucker <tophtucker@gmail.com>
2023-11-04 23:11:41 -0400

    more compact, no forced page breaks, default ignored list

--- dirToPandoc.js
+++ dirToPandoc.js
@@ -3,16 +3,16 @@ import path from "path";
 import {readFile} from "fs/promises";
 import {glob} from "glob";
 
-const allowedExtensions = ["js", "ts", "mjs", "json", "txt", "md"];
+// const allowedExtensions = ["js", "ts", "mjs", "json", "txt", "md"];
 
 const root = process.argv[2] || ".";
 const absoluteRoot = path.resolve(root);
-const title = process.argv[3] || root.split("/").filter(d => d.length).at(-1);
+const title = absoluteRoot.split("/").filter(d => d.length).at(-1);
 
 let doc = `<html>
 <title>${title}</title>
 <style>
-h1, h2, h3, h4, h5, h6 { margin-bottom: 1rem; page-break-before: always; }
+h1, h2, h3, h4, h5, h6 { margin-bottom: 1rem; }
 h6 { font-size: 1rem; }
 h5 { font-size: 2rem; }
 h4 { font-size: 4rem; }
@@ -24,9 +24,10 @@ h1 { font-size: 32rem; }
 
 <h1>${title}</h1>`;
 
-let ignore = [];
+// arbitrary initial ignore stuff, works for me, idk, i should let ppl pass this in
+let ignore = ["*.pdf", "package-lock.json", "yarn.lock", "*.png", "*.svg", "*.jpg"];
 try {
-  ignore = (await readFile(path.resolve(root, ".gitignore"), "utf8")).split("\n").filter(d => d);
+  ignore.push(...(await readFile(path.resolve(root, ".gitignore"), "utf8")).split("\n").filter(d => d));
 } catch (e) {
   // ignore
 }
@@ -44,13 +45,14 @@ function escape(s) {
 }
 
 async function getFiles(dir, level = 2) {
-  const dirents = await glob(dir + "/*", {cwd: absoluteRoot, ignore, withFileTypes: true});
+  const dirents = (await glob(dir + "/*", {cwd: absoluteRoot, ignore, withFileTypes: true}))
+    .sort((a, b) => a.name.localeCompare(b.name));
   for (const dirent of dirents) {
     const res = path.resolve(dir, dirent.name);
     if (dirent.isDirectory()) {
       doc += `<h${level}>${dirent.name}</h${level}>`;
       await getFiles(res, level + 1);
-    } else if (allowedExtensions.includes(res.split(".").at(-1))) {
+    } else {
       doc += `<h${level + 1}>${dirent.name}</h${level + 1}>
         <pre>${escape(await readFile(res, "utf8"))}</pre>`;
     }
