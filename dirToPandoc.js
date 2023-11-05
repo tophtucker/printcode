8b2bef51b812039cfa97119e3bcd34650197def6
Toph Tucker <tophtucker@gmail.com>
2023-11-04 21:02:36 -0400

    better and simpler, just writing html, no generator, no pandoc needed

--- dirToPandoc.js
+++ dirToPandoc.js
@@ -5,28 +5,36 @@ import {readdir, readFile} from "fs/promises";
 const allowedExtensions = ["js", "ts", "mjs", "json", "txt", "md"];
 
 const root = process.argv[2] || ".";
+const title = process.argv[3] || root.split("/").filter(d => d.length).at(-1);
+let doc = `<html><title>${title}</title><body>
 
-async function* getFiles(dir) {
+<h1>${title}</h1>`;
+
+// https://stackoverflow.com/a/30970751/120290
+function escape(s) {
+  let lookup = {
+      '&': "&amp;",
+      '"': "&quot;",
+      '\'': "&apos;",
+      '<': "&lt;",
+      '>': "&gt;"
+  };
+  return s.replace( /[&"'<>]/g, c => lookup[c] );
+}
+
+async function getFiles(dir, level = 2) {
   const dirents = await readdir(dir, {withFileTypes: true});
   for (const dirent of dirents) {
     const res = path.resolve(dir, dirent.name);
     if (dirent.isDirectory()) {
-      yield* getFiles(res);
-    } else {
-      yield res;
+      doc += `<h${level} style="page-break-before:always;">${res}</h${level}>${getFiles(res, level + 1)}`
+    } else if (allowedExtensions.includes(res.split(".").at(-1))) {
+      doc += `<h${level + 1}>${res}</h${level + 1}><pre>${escape(await readFile(res, "utf8"))}</pre>`;
     }
   }
 }
 
-(async () => {
-  let md = "# Title"
-  for await (const f of getFiles(root)) {
-    if (allowedExtensions.includes(f.split(".").at(-1))) {
-      md += ` \n\n## ${f}\n\n\`\`\``
-      md += (await readFile(f, "utf8"));
-      md += "\`\`\`"
-    }
-  }
-  md += "\n "
-  process.stdout.write(md);
-})();
+await getFiles(root);
+
+doc += "</body></html>";
+process.stdout.write(doc);
